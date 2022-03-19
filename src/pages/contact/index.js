9873eb7400e9bd33076fd44c07971deff040ecfe
Matt DuLeone <matt@duleone.com>
2022-03-18 23:37:17 -0400

    linktreeify contact links

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -16,12 +16,12 @@ const Contact = ({ contacts = [] }) => (
   <Layout>
     <Container>
       {contacts.map(({ href, icon, name }) => (
-        <li key={`${icon.join('-')}-${name}`}>
+        <ContactListItem key={`${icon.join('-')}-${name}`}>
           <ContactItem target="_blank" rel="noopener noreferrer" href={href}>
             <ContactName>{name}</ContactName>
             <Icon icon={icon} />
           </ContactItem>
-        </li>
+        </ContactListItem>
       ))}
     </Container>
   </Layout>
@@ -43,14 +43,11 @@ export default Contact;
 
 const Container = styled.ul`
   list-style: none;
+  padding: 0;
 `;
 
-const ContactItem = styled.a`
-  position: relative;
-`;
-
-const ContactName = styled.span`
-  margin-left: ${rem(24)};
+const ContactListItem = styled.li`
+  margin-bottom: ${rem(24)};
 `;
 
 const Icon = styled(FontAwesomeIcon)`
@@ -58,9 +55,42 @@ const Icon = styled(FontAwesomeIcon)`
   position: absolute;
   vertical-align: middle;
   width: ${rem(16)};
-  left: 0;
+  left: ${rem(24)};
 
   @media (prefers-color-scheme: dark) {
     color: ${({ theme }) => theme.colors.softWhite};
   }
+`;
+
+const ContactItem = styled.a`
+  position: relative;
+  display: block;
+  border: ${rem(1.5)} ${({ theme }) => theme.colors.black} solid;
+  border-radius: ${rem(6)};
+  padding: ${rem(16)} ${rem(24)};
+  margin: 0 auto;
+  transition: all 0.25s;
+
+  :focus,
+  :hover {
+    background-color: ${({ theme }) => theme.colors.duleoneRed};
+    color: ${({ theme }) => theme.colors.white};
+    border: ${rem(1.5)} ${({ theme }) => theme.colors.black} solid;
+
+    & ${Icon} {
+      color: ${({ theme }) => theme.colors.softWhite};
+    }
+
+    @media (prefers-color-scheme: dark) {
+      border: ${rem(1.5)} ${({ theme }) => theme.colors.softWhite} solid;
+    }
+  }
+
+  @media (prefers-color-scheme: dark) {
+    border-color: ${({ theme }) => theme.colors.softWhite};
+  }
+`;
+
+const ContactName = styled.span`
+  margin-left: ${rem(32)};
 `;
